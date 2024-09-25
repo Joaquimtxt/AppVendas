ac3a0eaff57a2ade819f75342588143412747335
Joaquimtxt <jocaq124@gmail.com>
2024-09-25 11:53:46 -0300

    Finalizada a view Index de produto

--- AppVendas/Models/Produto.cs
+++ AppVendas/Models/Produto.cs
@@ -1,13 +1,31 @@
-﻿namespace AppVendas.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AppVendas.Models
 {
     public class Produto
     {
         public Guid ProdutoId { get; set; }
+
+        [Display(Name = "Descrição")]
+        [Required(ErrorMessage = "O Campo é Obrigatório")]
+        [MaxLength(100, ErrorMessage = "O Campo deve ter, no máximo 100 caracteres!")]
+        [MinLength(3, ErrorMessage = "")]
+
         public string ProdutoNome { get; set; }
+        [Required(ErrorMessage = "O Campo é Obrigatório")]
+        [Range(0, double.MaxValue, ErrorMessage = "O valor do Produto deve ser um número positivo")]
+
         public double Valor { get; set; }
+        [Required(ErrorMessage = "O Campo é Obrigatório")]
+        [Range(0, double.MaxValue, ErrorMessage = "O valor do Produto deve ser um número positivo")]
+        [Display(Name ="Estoque Atual")]
+
         public double QtdadeEstoque { get; set; }
+        [Display(Name ="Ativo?")]
         public bool CadastroAtivo { get; set; }
 
+        /*Chave Estrangeira*/
+        [Required(ErrorMessage = "Por favor selecione uma categoria!")]
         public Guid CategoriaId { get; set; }
 
         public Categoria? Categoria { get; set; }
